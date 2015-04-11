8273325a14c54362dd925456534390943e177d79
TomasBC <tomasbentocosta@gmail.com>
2015-04-11 18:03:48 +0100

    anthill and ant model

--- Assets/Scripts/SpiderMovement.cs
+++ Assets/Scripts/SpiderMovement.cs
@@ -2,14 +2,13 @@
 using System.Collections;
 
 public class SpiderMovement : MonoBehaviour {
+	public int speed;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
 	}
 }
