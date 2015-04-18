7f2e8614d17371fa11816b7a825824d26a54f9a8
Filipe Relvas <Filipe@Filipes-MacBook-Pro.local>
2015-04-18 17:35:33 +0100

    House keeping

--- Assets/Scripts/Ant/ReactiveAnt.cs
+++ Assets/Scripts/Ant/ReactiveAnt.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class ReactiveAnt : MonoBehaviour {
 
 	private Rigidbody rb;
+	private GameObject food;
 
 	public float speed;
 	private int energy = 100;
 	private bool carrying = false;
-	private GameObject food;
-
+	
 	void Start() {
 		rb = GetComponent<Rigidbody>();
 	}
@@ -31,6 +31,8 @@ public class ReactiveAnt : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider collider){
+
+		//Load food
 		if (collider.gameObject.name.Contains("Orange")) {
 			food = collider.gameObject;
 			carrying = true;
@@ -38,28 +40,33 @@ public class ReactiveAnt : MonoBehaviour {
 	}
 
 	void OnCollisionEnter(Collision collision) {
+
+		//Wall collision
 		if (collision.gameObject.name.Contains("Wall")) {
 			RotateAnt();
-		} //Wall, Enemy... //Wall, Enemy...
+		}
 	}
 
 	//Reactors
 	private void Move() {
-		Vector3 pos;
+
+		//Move forward
 		rb.MovePosition(transform.position + transform.forward * speed * Time.deltaTime);
-		if (carrying) {
-			pos = transform.position + new Vector3(0f,1f,0f);
+	
+		//If carrying food move it as well
+		if (CarryingFood()) {
+			Vector3 pos = transform.position + new Vector3(0f,1f,0f);
 			food.transform.position = pos; 
 		}
 	}
 
 	private void RotateAnt(){
-		int rot = Random.Range (0, 3);
-		switch (rot) {
-		case 0 : rb.gameObject.transform.Rotate(0, -90f, 0); break;
-		case 1 : rb.gameObject.transform.Rotate(0, 90f, 0); break;
-		case 2 : rb.gameObject.transform.Rotate(0, 180f, 0); break;
-		case 3 : rb.gameObject.transform.Rotate(0, -180f, 0); break;
+
+		switch (Random.Range (0, 3)) {
+			case 0 : rb.gameObject.transform.Rotate(0, -90f, 0); break;
+			case 1 : rb.gameObject.transform.Rotate(0, 90f, 0); break;
+			case 2 : rb.gameObject.transform.Rotate(0, 180f, 0); break;
+			case 3 : rb.gameObject.transform.Rotate(0, -180f, 0); break;
 		}
 	}
 }
