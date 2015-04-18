be2d2e97e47d7206ad69bb154d583638244b8296
Filipe Relvas <Filipe@Filipes-MacBook-Pro.local>
2015-04-18 03:08:08 +0100

    Added movement and colision detection to the ant gameobject

--- Assets/Scripts/Ant/ReactiveAnt.cs
+++ Assets/Scripts/Ant/ReactiveAnt.cs
@@ -2,37 +2,39 @@
 using System.Collections;
 
 public class ReactiveAnt : MonoBehaviour {
-	private bool carrying = false;
-	private int energy = 100;
-	public GameObject ant;
-	public float speed = 1;
 
-	void Start () {
+	private Rigidbody rb;
+
+	public float speed;
+	private int energy = 100;
+	private bool carrying = false;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		move ();
+	void FixedUpdate() {
+		Move();
 	}
 
-	public void attacked(int value){
+	public void Attacked(int value) {
 		energy -= value;
 		if (energy < 0)
-			Destroy (ant);
+			Destroy(this);
 	}
 
 	//Sensors
-	public bool carryingFood(){
+	public bool CarryingFood() {
 		return carrying;
 	}
 
-	void OnTriggerEnter(Collider other){
-		Destroy (other.gameObject);
+	void OnCollisionEnter(Collision collision) {
+		//if (collision.gameObject.tag == "") {} //Wall, Enemy...
 	}
 
 	//Reactors
-	private void move(){
-		//Vector3 fw = ant.transform.position + ant.transform.forward * 0.3;
-		ant.transform.position += speed * ant.transform.forward * (float)Time.deltaTime;
+	private void Move() {
+		rb.AddForce(transform.forward * speed * Time.deltaTime);
 	}
 }
