1f4471e88b58fdc3e21570f1d2322f3ff7a0a34f
Pedro Lopes <pfmiralopes@gmail.com>
2015-05-08 21:03:52 +0100

    starting BDI architecture

---
+++ Assets/Scripts/BDI/BeliefsTypes.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum BeliefsTypes {
+	Attack,
+	CatchFood,
+	DropFood,
+	Run,
+	Follow
+}
---
+++ Assets/Scripts/Insects/Enemies/EnemyBDI.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyBDI : Insect {
+	private List<string> beliefs;
+	public float attackPower = 5.0f;
+	
+	// Initialization
+	protected override void Start() 
+	{
+		base.Start();
+		beliefs = new List<string> ();
+	}
+	
+	// Called every fixed framerate frame
+	protected override void FixedUpdate() 
+	{
+		base.FixedUpdate();
+		Move();
+	}
+	
+	// Sensors
+	protected override void OnCollisionEnter(Collision collision) {
+		
+		base.OnCollisionEnter(collision);
+		
+		//Ant?
+		if (collision.gameObject.name.Contains("ant")) {
+			collision.gameObject.GetComponent<Insect>().UpdateEnergy(-attackPower); //Attack
+			collided = true;
+		}
+		//Food?
+		if (collision.gameObject.name.Contains("food")) {
+			collided = true;
+		}
+	}
+	
+	// Reactors
+	protected override void Move() 
+	{
+		if (!collided) {
+			base.Move(); // Move forward
+			Rotate(randomMax);
+		} else {
+			Rotate(randomMin);
+			collided = false;
+		}
+	}
+}
